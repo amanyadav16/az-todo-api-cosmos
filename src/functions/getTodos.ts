ae330a08f581d5f8bf669b6d8c251cad11263123
aman.b.yadav <aman.b.yadav@accenture.com>
2024-04-16 17:37:23 +0530

    initial-setup

---
+++ src/functions/getTodos.ts
@@ -0,0 +1,15 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function getTodos(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('getTodos', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: getTodos
+});
